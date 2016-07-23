2ec6dcdc5dc6bb98309bcc5f4bd9b11ff59fc6e8
trix <trix@jc-mp.com>
2016-07-23 19:53:54 +0200

    Hello World

---
+++ src/main.rs
@@ -0,0 +1,204 @@
+extern crate syntex_syntax as syntax;
+extern crate rustc_serialize;
+extern crate clap;
+extern crate walkdir;
+
+mod visitor;
+
+use rustc_serialize::{json, Encodable, Encoder};
+
+use clap::{App, Arg, SubCommand, AppSettings};
+
+use syntax::parse::{self, ParseSess};
+use syntax::visit;
+
+use std::path::Path;
+
+#[derive(Debug)]
+enum MatchKind {
+    Struct,
+    Method,
+    Field,
+
+    Function,
+
+    Constant,
+    Static,
+    Enum,
+}
+
+// Required to convert the `MatchKind` enum fields to lowercase in the JSON output
+impl Encodable for MatchKind {
+    fn encode<E: Encoder>(&self, e: &mut E) -> Result<(), E::Error> {
+        try!(e.emit_str(match *self {
+            MatchKind::Struct => "struct",
+            MatchKind::Method => "method",
+            MatchKind::Field => "field",
+
+            MatchKind::Function => "function",
+
+            MatchKind::Constant => "constant",
+            MatchKind::Static => "static",
+            MatchKind::Enum => "enum",
+        }));
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, RustcEncodable)]
+pub struct Match {
+    path: String,
+    name: String,
+    container: String,
+    kind: MatchKind,
+    line: usize,
+}
+
+#[derive(PartialEq)]
+enum SearchType {
+    // Limited to the specific rust file provided
+    Local,
+    // Limited to the specific rust file provided and any imported modules
+    // This is the default search option
+    LocalChildren,
+    // Searches the entire crate root for any *.rs files
+    Global,
+}
+
+fn dump_ast<'a>(matches: &clap::ArgMatches<'a>) {
+    let file = matches.value_of("file").unwrap();
+
+    let session = ParseSess::new();
+    let krate = parse::parse_crate_from_file(file.as_ref(), vec![], &session).unwrap();
+
+    // Pretty print the parsed AST
+    println!("{:#?}", krate.module);
+}
+
+fn search_symbol_global(path: &str, query: &str) -> Vec<Match> {
+    use walkdir::WalkDir;
+
+    let crate_root = Path::new(path);
+
+    // Check for the presence of Cargo.toml. When searching globally, the user should search from
+    // the crate root as we make some assumptions about the environment.
+    match std::fs::metadata(crate_root.join("Cargo.toml")) {
+        Ok(_) => {}
+        Err(_) => {
+            panic!("Failed to open Cargo.toml. When searching globally, you must begin at the \
+                    crate root")
+        }
+    };
+
+    let target_dir = crate_root.join("target");
+
+    let mut matches: Vec<Match> = Vec::new();
+
+    for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
+        let path = entry.path();
+
+        // Filter out target directory
+        if path.starts_with(&target_dir) {
+            continue;
+        }
+
+        let filename = match path.file_name().and_then(|s| s.to_str()) {
+            Some(s) => s,
+            None => continue,
+        };
+
+        // Filter out anything that isn't a rust source unit
+        if !filename.ends_with(".rs") {
+            continue;
+        }
+
+        if let Some(path) = path.to_str() {
+            matches.append(&mut search_symbol_file(path, query, false));
+        }
+    }
+
+    matches
+}
+
+fn search_symbol_file(file: &str, query: &str, search_children: bool) -> Vec<Match> {
+    let session = ParseSess::new();
+    let krate = match parse::parse_crate_from_file(file.as_ref(), vec![], &session) {
+        Ok(krate) => krate,
+        Err(_) => return vec![],
+    };
+
+    use visitor::SymbolVisitor;
+    let mut visitor = SymbolVisitor {
+        matches: vec![],
+        codemap: session.codemap(),
+        search_children: search_children,
+        query: query.into(),
+    };
+
+    visit::walk_crate(&mut visitor, &krate);
+
+    visitor.matches
+}
+
+fn search_symbol<'a>(matches: &clap::ArgMatches<'a>) -> Vec<Match> {
+    let file = matches.value_of("file").unwrap();
+    let query = matches.value_of("query").unwrap_or_default();
+
+    let mut search_type = SearchType::LocalChildren;
+    if matches.is_present("local") {
+        search_type = SearchType::Local;
+    } else if matches.is_present("global") {
+        search_type = SearchType::Global;
+    }
+
+    match search_type {
+        SearchType::Global => search_symbol_global(file, query),
+        SearchType::Local => search_symbol_file(file, query, false),
+        SearchType::LocalChildren => search_symbol_file(file, query, true),
+    }
+}
+
+fn main() {
+    let app = App::new("rust-symbols")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .settings(&[AppSettings::GlobalVersion, AppSettings::SubcommandRequiredElseHelp])
+        .subcommand(SubCommand::with_name("ast")
+            .about("Pretty print the AST for the provided rust source file")
+            .setting(AppSettings::ArgRequiredElseHelp)
+            .arg(Arg::with_name("file")
+                .required(true)
+                .help("The rust source file to print the AST for")))
+        .subcommand(SubCommand::with_name("search")
+            .about("Search for a symbol")
+            .setting(AppSettings::ArgRequiredElseHelp)
+            .arg(Arg::with_name("local")
+                .long("local")
+                .short("l")
+                .help("Search for symbols found only in the provided rust source file"))
+            .arg(Arg::with_name("global")
+                .long("global")
+                .short("g")
+                .conflicts_with("local")
+                .help("Search for symbols found in the entire crate"))
+            .arg(Arg::with_name("file")
+                .required(true)
+                .help("The rust source file or crate root to search for symbols in"))
+            .arg(Arg::with_name("query").help("Optional string to match symbols against")));
+    let matches = app.get_matches();
+
+    if let (cmd, Some(m)) = matches.subcommand() {
+        match cmd {
+            "ast" => dump_ast(m),
+            "search" => {
+                let matches = search_symbol(m);
+
+                let encoded = json::encode(&matches).unwrap();
+                println!("{}", encoded);
+            }
+            _ => unreachable!(),
+        }
+    }
+}
---
+++ src/visitor.rs
@@ -0,0 +1,163 @@
+use {Match, MatchKind};
+
+use syntax::ast;
+use syntax::codemap::{CodeMap, Span};
+use syntax::visit::{self, FnKind, Visitor};
+
+pub struct SymbolVisitor<'a> {
+    pub matches: Vec<Match>,
+    pub codemap: &'a CodeMap,
+    pub search_children: bool,
+    pub query: String,
+}
+
+impl<'a> SymbolVisitor<'a> {
+    fn create_match(&mut self, name: &str, container: Option<&str>, kind: MatchKind, span: Span) {
+        // Check if all of the characters in the query are present in the symbol
+        if self.query.chars().any(|c| !name.to_lowercase().contains(c)) {
+            return;
+        }
+
+        let filename = self.codemap.span_to_filename(span);
+        let line = self.codemap.lookup_char_pos(span.lo).line;
+
+        self.matches.push(Match {
+            path: filename,
+            name: name.into(),
+            container: container.unwrap_or_default().into(),
+            kind: kind,
+            line: line,
+        });
+    }
+}
+
+fn get_ident_name(ident: &ast::Ident) -> String {
+    ident.name.as_str().to_string()
+}
+
+impl<'a> Visitor for SymbolVisitor<'a> {
+    // Catch free standing functions
+    fn visit_fn(&mut self,
+                fn_kind: FnKind,
+                fn_decl: &ast::FnDecl,
+                block: &ast::Block,
+                span: Span,
+                _: ast::NodeId) {
+        let fn_name = match fn_kind {
+            FnKind::ItemFn(id, _, _, _, _, _) => Some(get_ident_name(&id)),
+            _ => None,
+        };
+
+        if let Some(name) = fn_name {
+            self.create_match(&name, None, MatchKind::Function, span);
+        }
+
+        visit::walk_fn(self, fn_kind, fn_decl, block, span);
+    }
+
+    // Catch pretty much everything else
+    fn visit_item(&mut self, item: &ast::Item) {
+        use syntax::ast::ItemKind;
+        use syntax::ast::VariantData;
+        use syntax::ast::TraitItemKind;
+        use syntax::ast::ImplItemKind;
+
+        let item_name = get_ident_name(&item.ident);
+
+        match item.node {
+            ItemKind::Mod(_) => {
+                // Stop us from going into dependent modules
+                if !self.search_children {
+                    return;
+                }
+            }
+
+            ItemKind::Struct(ref variant, _) => {
+                self.create_match(&item_name, None, MatchKind::Struct, item.span);
+
+                match variant {
+                    &VariantData::Struct(ref fields, _) |
+                    &VariantData::Tuple(ref fields, _) => {
+                        for field in fields {
+                            if let Some(field_ident) = field.ident {
+                                let field_name = get_ident_name(&field_ident);
+
+                                self.create_match(&field_name,
+                                                  Some(&item_name),
+                                                  MatchKind::Field,
+                                                  field.span);
+                            }
+                        }
+                    }
+                    _ => {}
+                };
+            }
+
+            ItemKind::Trait(_, _, _, ref items) => {
+                self.create_match(&item_name, None, MatchKind::Struct, item.span);
+
+                for v in items {
+                    let trait_item_name = get_ident_name(&v.ident);
+
+                    let kind = match v.node {
+                        TraitItemKind::Method(_, _) => Some(MatchKind::Method),
+                        TraitItemKind::Const(_, _) => Some(MatchKind::Constant),
+                        _ => None,
+                    };
+
+                    if let Some(kind) = kind {
+                        self.create_match(&trait_item_name, Some(&item_name), kind, v.span);
+                    }
+                }
+            }
+
+            ItemKind::Enum(ref def, _) => {
+                self.create_match(&item_name, None, MatchKind::Enum, item.span);
+
+                for v in &def.variants {
+                    let variant_name = get_ident_name(&v.node.name);
+
+                    self.create_match(&variant_name, Some(&item_name), MatchKind::Constant, v.span);
+                }
+            }
+
+            ItemKind::Const(_, _) => {
+                self.create_match(&item_name, None, MatchKind::Constant, item.span);
+            }
+
+            ItemKind::Static(_, _, _) => {
+                self.create_match(&item_name, None, MatchKind::Static, item.span);
+            }
+
+            ItemKind::Impl(_, _, _, _, ref ty, ref items) => {
+                let mut struct_name = String::new();
+
+                // Figure out the struct name on the right hand side of the `impl` expression
+                match ty.node {
+                    ast::TyKind::Path(_, ref p) => {
+                        struct_name = get_ident_name(&p.segments[0].identifier);
+                    }
+                    _ => {}
+                };
+
+                // Now populate the methods
+                for item in items {
+                    match item.node {
+                        ImplItemKind::Method(_, _) => {
+                            self.create_match(&get_ident_name(&item.ident),
+                                              Some(&struct_name),
+                                              MatchKind::Method,
+                                              item.span)
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        };
+
+        visit::walk_item(self, item);
+    }
+
+    fn visit_mac(&mut self, _mac: &ast::Mac) {}
+}
